c9dc820490944fbe90c856f52d779965c237178d
Jordan Harband <ljharb@gmail.com>
2025-02-06 21:59:27 -0800

    [Breaking] complete overhaul; use ESM and modern syntax

---
+++ index.mjs
@@ -0,0 +1,121 @@
+import { readFile, writeFile } from 'fs/promises';
+import { readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
+import extend from 'node.extend';
+
+function checkKey(key) {
+	if ((typeof key !== 'number' || !isFinite(key)) && (typeof key !== 'string' || key.length === 0)) {
+		throw new TypeError('key must be a finite number or a nonempty string');
+	}
+}
+
+function isPlainObject(value) {
+	if (!value || typeof value !== 'object' || Array.isArray(value)) {
+		return false;
+	}
+	var proto = Object.getPrototypeOf(value);
+	return proto === Object.prototype || proto === null;
+}
+
+export class JSONData {
+	format;
+
+	constructor(raw) {
+		const hasTrailingNewline = (/\n\n$/).test(raw);
+		const indentMatch = String(raw).match(/^[ \t]+/m);
+		const indent = (indentMatch && indentMatch[0]) || 2;
+
+		if (raw) {
+			var result = JSON.parse(raw);
+			if (result && Object(result) === result) {
+				this.data = result;
+			}
+		}
+
+		this.format = {
+			__proto__: null,
+			indent,
+			trailing: hasTrailingNewline,
+		};
+	}
+
+	async get(key) {
+		const value = arguments.length > 0 ? this.data[key] : { ...this.data };
+
+		return isPlainObject(value) ? { ...value } : value;
+	}
+
+	set(obj) {
+		if (!isPlainObject(obj)) {
+			throw new TypeError('object must be a plain object');
+		}
+		extend(true, this.data, obj);
+	}
+
+	remove(key) {
+		checkKey(key);
+
+		const status = delete this.data[key];
+		if (!status) {
+			throw new Error('deletion failed');
+		}
+	}
+
+	stringify() {
+		const { trailing, indent } = this.format;
+		const endingNewlines = trailing ? '\n\n' : '\n';
+		return JSON.stringify(this.data, null, indent) + endingNewlines;
+	}
+}
+
+export class JSONFile extends JSONData {
+	#filename;
+
+	filename;
+
+	constructor(filename, raw) {
+		super(raw);
+
+		this.#filename = filename;
+		Object.defineProperty(this, 'filename', {
+			configurable: false,
+			value: filename,
+			writable: false,
+		});
+
+		const { format } = this;
+		delete this.format;
+		this.format = format;
+	}
+
+	// eslint-disable-next-line no-dupe-class-members -- this is so the function can be borrowed if needed
+	get filename() {
+		return this.#filename;
+	}
+
+	async save() {
+		return writeFile(this.#filename, this.stringify());
+	}
+
+	saveSync() {
+		writeFileSync(this.#filename, this.stringify());
+	}
+}
+
+export function sync(filename) {
+	const raw = readFileSync(filename, 'utf8');
+
+	return new JSONFile(filename, raw);
+}
+
+export default async function readJSON(filename) {
+	const raw = await readFile(resolve(filename), { encoding: 'utf8' });
+
+	return new JSONFile(filename, raw);
+}
+
+readJSON.sync = sync;
+readJSON.JSONData = JSONData;
+readJSON.JSONFile = JSONFile;
+
+export { readJSON as 'module.exports' };
---
+++ test/index.mjs
@@ -0,0 +1,241 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import test from 'tape';
+import hasOwn from 'hasown';
+import v from 'es-value-fixtures';
+
+import jsonFile, { sync, JSONFile, JSONData } from '../index.mjs';
+
+const noNewlineFilename = 'test/no-trailing-newline.json';
+const testFilename = 'test/test.json';
+const testContents = {
+	arr: [1, 2, 3],
+	'false': false,
+	foo: 'bar',
+	'null': null,
+	obj: { nested: {} },
+	'true': true,
+};
+
+const NODE_011_NOT_FOUND = -2;
+const NODE_010_NOT_FOUND = 34;
+const WINDOWS_NODE_7_NOT_FOUND = -4058;
+function isFileNotFoundError(err) {
+	return [
+		NODE_010_NOT_FOUND,
+		NODE_011_NOT_FOUND,
+		WINDOWS_NODE_7_NOT_FOUND,
+	].indexOf(err.errno) > -1;
+}
+
+function enoent(err, filename) {
+	const message = 'ENOENT' + (err.message.indexOf('no such file or directory') === 8 ? ': no such file or directory' : '') + ', open \'' + filename + "'";
+	const expectedError = Object.assign(new Error(message), {
+		code: 'ENOENT',
+		errno: err.errno,
+		path: filename,
+	});
+	if (hasOwn(err, 'syscall')) {
+		expectedError.syscall = 'open';
+	}
+	return expectedError;
+}
+
+test('returns a file', async (t) => {
+	const file = await jsonFile(testFilename);
+
+	t.ok(file instanceof JSONFile, 'file is instance of JSONFile');
+	t.ok(file instanceof JSONData, 'file is instance of JSONData');
+});
+
+test('returns an exception if the file is not found', async (t) => {
+	try {
+		await jsonFile('NOT A REAL FILE');
+		t.fail();
+	} catch (err) {
+		t.ok(err, 'error is truthy');
+		t.ok(isFileNotFoundError(err), 'error number is correct');
+		t.deepEqual(err, enoent(err, path.resolve('NOT A REAL FILE')), 'returns an error');
+	}
+});
+
+test('returns an exception if the file has invalid JSON', async (t) => {
+	try {
+		await jsonFile(import.meta.filename || fileURLToPath(import.meta.url));
+		t.fail();
+	} catch (err) {
+		t.ok(err instanceof SyntaxError, 'error is a SyntaxError');
+		t.equal(typeof err.message, 'string', 'err.message is a string');
+		const expected = /^Unexpected token ['i]/;
+		t.match(err.message, expected, 'gives the expected error');
+	}
+});
+
+test('format', async (t) => {
+	const file = await jsonFile(testFilename);
+
+	t.equal(file.format.indent, '\t', 'reads tabs');
+	t.equal(file.format.trailing, true, 'reads trailing newline');
+	t.deepEqual(
+		file.format,
+		{ __proto__: null, indent: '\t', trailing: true },
+		'entire format is properly read',
+	);
+
+	t.test('no trailing newline', async (st) => {
+		const noNewlineFile = await jsonFile(noNewlineFilename);
+
+		st.notOk(noNewlineFile.format.trailing, 'reads no trailing newline');
+		st.equal(noNewlineFile.format.indent, '   ', 'reads three spaces');
+	});
+});
+
+test('#get(): file.data', async (st) => {
+	const file = await jsonFile(testFilename);
+	st.deepEqual(file.data, testContents, 'file.data matches expected');
+
+	const value = await file.get('obj');
+	st.notEqual(value, file.data.obj, 'get(key)->object is not the same reference');
+});
+
+test('#get(): with key', async (st) => {
+	const file = await jsonFile(testFilename);
+
+	return Promise.all(Object.entries(testContents).map(async ([key, keyContents]) => {
+		const value = await file.get(key);
+		st.deepEqual(value, keyContents, 'data from get("' + key + '") matches');
+	}));
+});
+
+test('#get(): without key', async (s2t) => {
+	const file = await jsonFile(testFilename);
+
+	const getData = await file.get();
+
+	s2t.deepEqual(getData, file.data, 'data from get() matches');
+	s2t.notEqual(getData, file.data, 'data from get() is not the same reference');
+});
+
+test('#remove()', (st) => {
+	st.test('with key', async (s2t) => {
+		const file = await jsonFile(testFilename);
+		const result = await file.remove('arr');
+
+		s2t.equal(result, undefined, 'deletion successful');
+		s2t.equal('arr' in file.data, false, 'key removed from data');
+	});
+
+	st.test('with an empty key', async (s2t) => {
+		const file = await jsonFile(testFilename);
+		try {
+			await file.remove('');
+			s2t.fail();
+		} catch (err) {
+			s2t.equal(err instanceof TypeError, true, 'err is TypeError');
+		}
+	});
+
+	st.end();
+});
+
+test('#set()', async (t) => {
+	const file = await jsonFile(testFilename);
+
+	t.equal(undefined, file.data.foobar, 'foo starts undefined');
+	const data = {
+		foobar: {
+			bar: 'baz',
+			quux: true,
+		},
+	};
+	file.set(data);
+	t.deepEqual(file.data.foobar, data.foobar, 'expected data is set');
+	t.notEqual(file.data.foobar, data.foobar, 'data is not the same reference');
+});
+
+test('#set(): setting invalid data', async (st) => {
+	const file = await jsonFile(testFilename);
+
+	const error = new TypeError('object must be a plain object');
+
+	st.throws(() => { file.set(null); }, error, 'throws when given non-object');
+	st.throws(() => { file.set(true); }, error, 'throws when given non-object');
+	st.throws(() => { file.set([]); }, error, 'throws when given non-object');
+	st.throws(() => { file.set(() => {}); }, error, 'throws when given non-object');
+	st.throws(() => { file.set('foo'); }, error, 'throws when given non-object');
+	st.throws(() => { file.set(/f/); }, error, 'throws when given non-object');
+});
+
+test('returns an error when no file', async (t) => {
+	const filename = path.join(process.cwd(), 'does not exist.json');
+	try {
+		await jsonFile(filename);
+		t.fail();
+	} catch (err) {
+		t.ok(err, 'error is truthy');
+		t.ok(isFileNotFoundError(err), 'error number is correct');
+		t.deepEqual(err, enoent(err, filename), 'returned an error');
+	}
+});
+
+test('remembers filename', async (t) => {
+	const file = await jsonFile(testFilename);
+
+	t.equal(file.filename, testFilename, 'filename equals ' + testFilename);
+});
+
+test('saves properly', async (t) => {
+	const file = await jsonFile(testFilename);
+
+	t.equal(file.filename, testFilename, 'filename equals ' + testFilename);
+	file.set({ foo: !testContents.foo });
+
+	await file.save();
+
+	const file2 = await jsonFile(testFilename);
+
+	const value = await file2.get('foo');
+	t.equal(value, !testContents.foo, 'value was properly saved');
+	file2.set({ foo: testContents.foo }); // restore original value
+
+	await file2.save();
+});
+
+test('#saveSync', (t) => {
+	const file = sync(testFilename);
+	file.set({ foo: !testContents.foo });
+	try {
+		file.saveSync();
+		t.ok(true, 'saveSync: success');
+	} finally {
+		file.set({ foo: testContents.foo });
+		file.saveSync();
+		t.ok(true, 'saveSync, restore original: success');
+
+		t.end();
+	}
+});
+
+test('sync', (t) => {
+	t.throws(() => { sync('not a filename'); }, 'nonexistent filename throws');
+
+	const file = sync(testFilename);
+
+	t.deepEqual(file.data, testContents, 'sync file data is expected data');
+	t.equal(true, file instanceof JSONFile, 'file is JSONFile');
+	t.equal(true, file instanceof JSONData, 'file is JSONData');
+
+	t.end();
+});
+
+test('JSONData', (t) => {
+	v.primitives.forEach(function (primitive) {
+		if (typeof primitive !== 'undefined' && typeof primitive !== 'bigint' && typeof primitive !== 'symbol') {
+			var data = new jsonFile.JSONData(JSON.stringify(primitive));
+			t.notOk(data && 'data' in data, 'raw that parses to non-object (' + primitive + ') noops');
+		}
+	});
+
+	t.end();
+});
